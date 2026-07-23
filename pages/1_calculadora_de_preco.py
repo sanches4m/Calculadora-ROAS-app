6313f17c450f70c7180b34088030304f77340505
sanches4m <121514425+sanches4m@users.noreply.github.com>
2026-07-23 13:36:30 -0300

    1_calculadora_de_preco.py

--- pages/1_calculadora_de_preco.py
+++ pages/1_calculadora_de_preco.py
@@ -6,7 +6,7 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from estilos import aplicar_estilo_visual
 
-aplicar_estilo_visual()
+# aplicar_estilo_visual()
 
 st.title("💰 Calculadora de Preço e Margem")
 st.markdown("Analise a saúde do seu preço atual e simule cenários para aumentar sua margem.")
